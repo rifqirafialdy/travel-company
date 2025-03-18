285f488d0bb2e0be45a48e11e311d72111ee8a6b
rifqirafialdy <rifqirafialdy@gmail.com>
2025-03-19 05:18:04 +0700

    teams module fix

---
+++ app/Teams/page.tsx
@@ -0,0 +1,11 @@
+import AboutUsContent from "@/Components/About-Us";
+import Header from "@/Components/Header";
+
+export default function Teams(){
+    return(
+        <div>
+            <Header/>
+            <AboutUsContent/>
+        </div>
+    )
+}
